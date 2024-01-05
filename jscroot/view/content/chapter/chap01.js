9dddf7e592bbf526b303b3c98c2da7cfe0d5b66a
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2024-01-05 11:27:54 +0700

    updatesd

--- jscroot/view/content/chapter/chap01.js
+++ jscroot/view/content/chapter/chap01.js
@@ -1,5 +1,6 @@
 import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.2/croot.js";
 
 export function main(){
-    setInner("biggreet","Halo Ngadimin");
+    setInner("Sankuy","Halo Ngadimin");
+    addGeoJSONToMapAndTable('', map, document.querySelector('table'));
 }
